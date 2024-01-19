ca284a69b7bbe4646a489ee753704ad5e4601c0f
Kevin G <kevinfgoyesa@gmail.com>
2024-01-19 20:16:03 +1100

    Se incluyo logo y video

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const input = document.querySelector ("input");
 const h3 = document.querySelector ("h3")
 const imagen1 = document.getElementById("imagen1")
-
+const imagen2 = document.getElementById("imagen2")
 
